165b505aa63599a703bddaae6bc1a1d722070ee3
Hunter Gerlachv <hunter@huntergerlach.com>
2024-11-06 19:05:11 -0500

    Reset next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/<repo-name>", // Replace with your GitHub repository name if using GitHub Pages
+  /* config options here */
 };
 
 export default nextConfig;
