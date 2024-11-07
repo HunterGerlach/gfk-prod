a016cc61af749e5b55c861d670dc2965e2362710
Hunter Gerlachv <hunter@huntergerlach.com>
2024-11-06 19:04:18 -0500

    Update workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/<repo-name>", // Replace with your GitHub repository name if using GitHub Pages
 };
 
 export default nextConfig;
